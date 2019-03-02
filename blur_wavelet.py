c0327b4edcf104d617372f05ed8f27acfd1c22f7
PedrofRodenas <pedrofrodenas@gmail.com>
2019-03-02 20:01:29 +0100

    Blur stimation using haar wavelet transform

---
+++ blur_wavelet.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  1 20:22:32 2019
+
+@author: pedrofRodenas
+"""
+
+import pywt
+import cv2
+import numpy as np
+import os
+import argparse
+import json
+
+def blur_detect(img, threshold):
+    
+    
+    Y = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    
+    
+    M, N = Y.shape
+    
+    # Crop input image to be 3 divisible by 2
+    Y = Y[0:int(M/16)*16, 0:int(N/16)*16]
+    
+    LL1,(LH1,HL1,HH1)= pywt.dwt2(Y, 'haar')
+    ## Another application of 2D haar to LL1
+    LL2,(LH2,HL2,HH2)= pywt.dwt2(LL1, 'haar') 
+    ## Another application of 2D haar to LL2
+    LL3,(LH3,HL3,HH3)= pywt.dwt2(LL2, 'haar')
+    
+    # Construct the edge map in each scale
+    E1 = np.sqrt(np.power(LH1, 2)+np.power(HL1, 2)+np.power(HH1, 2))
+    E2 = np.sqrt(np.power(LH2, 2)+np.power(HL2, 2)+np.power(HH2, 2))
+    E3 = np.sqrt(np.power(LH3, 2)+np.power(LH3, 2)+np.power(HH3, 2))
+    
+    M1, N1 = E1.shape
+
+    # Sliding window size
+    sizeM1 = 8
+    sizeN1 = 8
+    
+    sizeM2 = int(sizeM1/2)
+    sizeN2 = int(sizeN1/2)
+    
+    sizeM3 = int(sizeM2/2)
+    sizeN3 = int(sizeN2/2)
+    
+    # Number of edge maps
+    N_iter = int((M1/sizeM1)*(N1/sizeN1))
+    
+    Emax1 = np.zeros((N_iter))
+    Emax2 = np.zeros((N_iter))
+    Emax3 = np.zeros((N_iter))
+    
+    # Get the maximum value of slicing windows over edge maps
+    count = 0
+    
+    # Sliding windows index of level 1
+    x1 = 0
+    y1 = 0
+    # Sliding windows index of level 2
+    x2 = 0
+    y2 = 0
+    # Sliding windows index of level 3
+    x3 = 0
+    y3 = 0
+    
+    Y_limit = N1-sizeN1
+    
+    while count < N_iter:
+        
+        Emax1[count] = np.max(E1[x1:x1+sizeM1,y1:y1+sizeN1])
+        Emax2[count] = np.max(E2[x2:x2+sizeM2,y2:y2+sizeN2])
+        Emax3[count] = np.max(E3[x3:x3+sizeM3,y3:y3+sizeN3])
+        
+        if y1 == Y_limit:
+            x1 = x1 + sizeM1
+            y1 = 0
+            
+            x2 = x2 + sizeM2
+            y2 = 0
+            
+            x3 = x3 + sizeM3
+            y3 = 0
+            
+            count += 1
+            
+        else:
+                
+            y1 = y1 + sizeN1
+            y2 = y2 + sizeN2
+            y3 = y3 + sizeN3
+            count += 1
+    
+    EdgePoint1 = Emax1 > threshold;
+    EdgePoint2 = Emax2 > threshold;
+    EdgePoint3 = Emax3 > threshold;
+    
+    # Rule 1 Edge Pojnts
+    EdgePoint = EdgePoint1 + EdgePoint2 + EdgePoint3
+    
+    n_edges = EdgePoint.shape[0]
+    
+    # Rule 2 Dirak-Structure or Astep-Structure
+    DAstructure = (Emax1[EdgePoint] > Emax2[EdgePoint]) * (Emax2[EdgePoint] > Emax3[EdgePoint]);
+    
+    # Rule 3 Roof-Structure or Gstep-Structure
+    
+    RGstructure = np.zeros((n_edges))
+
+    for i in range(n_edges):
+    
+        if EdgePoint[i] == 1:
+        
+            if Emax1[i] < Emax2[i] and Emax2[i] < Emax3[i]:
+            
+                RGstructure[i] = 1
+                
+    # Rule 4 Roof-Structure
+    
+    RSstructure = np.zeros((n_edges))
+
+    for i in range(n_edges):
+    
+        if EdgePoint[i] == 1:
+        
+            if Emax2[i] > Emax1[i] and Emax2[i] > Emax3[i]:
+            
+                RSstructure[i] = 1
+
+    # Rule 5 Edge more likely to be in a blurred image 
+
+    BlurC = np.zeros((n_edges));
+
+    for i in range(n_edges):
+    
+        if RGstructure[i] == 1 or RSstructure[i] == 1:
+        
+            if Emax1[i] < threshold:
+            
+                BlurC[i] = 1                        
+        
+    # Step 6
+    Per = np.sum(DAstructure)/np.sum(EdgePoint)
+    
+    if np.sum(RSstructure) == 0:
+        
+        BlurExtent = 100
+    else:
+        BlurExtent = np.sum(BlurC)/np.sum(RSstructure)
+    
+    return Per, BlurExtent
+
+
+def find_images(input_dir):
+    extensions = [".jpg", ".png", ".jpeg"]
+
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            if os.path.splitext(file)[1].lower() in extensions:
+                yield os.path.join(root, file)
+                
+                
+if __name__ == '__main__':                
+    parser = argparse.ArgumentParser(description='run blur detection on a single image')
+    parser.add_argument('-i', '--input_dir', dest="input_dir", type=str, required=True, help="directory of images")
+    parser.add_argument('-s', '--save_path', dest='save_path', type=str, help="path to save output")
+    parser.add_argument("-t", "--threshold", dest='threshold', type=float, default=35, help="blurry threshold")
+    parser.add_argument("-d", "--decision", dest='decisionthreshold', type=float, default=0.001, help="Decision Threshold")
+    args = parser.parse_args()
+    
+    results = []
+    
+    for input_path in find_images(args.input_dir):
+        try:
+            #logging.info("processing {0}".format(input_path))
+            I = cv2.imread(input_path)
+            per, blurext = blur_detect(I, args.threshold)
+            if per < args.threshold:
+                classification = True
+            else:
+                classification = False
+            results.append({"input_path": input_path, "per": per, "blur extent": blurext, "is blur": classification})
+            print("{0}, Per: {1}, blur extent: {2}, is blur: {3}".format(input_path, per, blurext, classification))
+            
+            
+        except Exception as e:
+            print(e)
+            pass
+        
+    if args.save_path:
+        
+        with open(args.save_path, 'w') as outfile:
+            json.dump(results, outfile, sort_keys=True, indent=4)
+            outfile.write("\n")
+        
+        
+        
+    
+
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
