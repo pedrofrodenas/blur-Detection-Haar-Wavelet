ddc5dfb9d573a5c12a33c53bbe13c02299bba0d7
PedrofRodenas <pedrofrodenas@gmail.com>
2019-03-02 21:09:36 +0100

    some coments

--- blur_wavelet.py
+++ blur_wavelet.py
@@ -15,7 +15,7 @@ import json
 
 def blur_detect(img, threshold):
     
-    
+    # Convert image to grayscale
     Y = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     
     
@@ -24,37 +24,40 @@ def blur_detect(img, threshold):
     # Crop input image to be 3 divisible by 2
     Y = Y[0:int(M/16)*16, 0:int(N/16)*16]
     
+    # Step 1, compute Haar wavelet of input image
     LL1,(LH1,HL1,HH1)= pywt.dwt2(Y, 'haar')
-    ## Another application of 2D haar to LL1
+    # Another application of 2D haar to LL1
     LL2,(LH2,HL2,HH2)= pywt.dwt2(LL1, 'haar') 
-    ## Another application of 2D haar to LL2
+    # Another application of 2D haar to LL2
     LL3,(LH3,HL3,HH3)= pywt.dwt2(LL2, 'haar')
     
-    # Construct the edge map in each scale
+    # Construct the edge map in each scale Step 2
     E1 = np.sqrt(np.power(LH1, 2)+np.power(HL1, 2)+np.power(HH1, 2))
     E2 = np.sqrt(np.power(LH2, 2)+np.power(HL2, 2)+np.power(HH2, 2))
     E3 = np.sqrt(np.power(LH3, 2)+np.power(LH3, 2)+np.power(HH3, 2))
     
     M1, N1 = E1.shape
 
-    # Sliding window size
+    # Sliding window size level 1
     sizeM1 = 8
     sizeN1 = 8
     
+    # Sliding windows size level 2
     sizeM2 = int(sizeM1/2)
     sizeN2 = int(sizeN1/2)
     
+    # Sliding windows size level 3
     sizeM3 = int(sizeM2/2)
     sizeN3 = int(sizeN2/2)
     
-    # Number of edge maps
+    # Number of edge maps, related to sliding windows size
     N_iter = int((M1/sizeM1)*(N1/sizeN1))
     
     Emax1 = np.zeros((N_iter))
     Emax2 = np.zeros((N_iter))
     Emax3 = np.zeros((N_iter))
     
-    # Get the maximum value of slicing windows over edge maps
+    
     count = 0
     
     # Sliding windows index of level 1
@@ -67,14 +70,19 @@ def blur_detect(img, threshold):
     x3 = 0
     y3 = 0
     
+    # Sliding windows limit of horizontal dimension
     Y_limit = N1-sizeN1
     
     while count < N_iter:
-        
+        # Get the maximum value of slicing windows over edge maps 
+        # in each level
         Emax1[count] = np.max(E1[x1:x1+sizeM1,y1:y1+sizeN1])
         Emax2[count] = np.max(E2[x2:x2+sizeM2,y2:y2+sizeN2])
         Emax3[count] = np.max(E3[x3:x3+sizeM3,y3:y3+sizeN3])
         
+        # if sliding windows ends horizontal direction
+        # move along vertical direction and resets horizontal
+        # direction
         if y1 == Y_limit:
             x1 = x1 + sizeM1
             y1 = 0
@@ -86,7 +94,8 @@ def blur_detect(img, threshold):
             y3 = 0
             
             count += 1
-            
+        
+        # windows moves along horizontal dimension
         else:
                 
             y1 = y1 + sizeN1
@@ -94,6 +103,7 @@ def blur_detect(img, threshold):
             y3 = y3 + sizeN3
             count += 1
     
+    # Step 3
     EdgePoint1 = Emax1 > threshold;
     EdgePoint2 = Emax2 > threshold;
     EdgePoint3 = Emax3 > threshold;
@@ -145,6 +155,7 @@ def blur_detect(img, threshold):
     # Step 6
     Per = np.sum(DAstructure)/np.sum(EdgePoint)
     
+    # Step 7
     if np.sum(RSstructure) == 0:
         
         BlurExtent = 100
@@ -164,7 +175,7 @@ def find_images(input_dir):
                 
                 
 if __name__ == '__main__':                
-    parser = argparse.ArgumentParser(description='run blur detection on a single image')
+    parser = argparse.ArgumentParser(description='run Haar Wavelet blur detection on a folder')
     parser.add_argument('-i', '--input_dir', dest="input_dir", type=str, required=True, help="directory of images")
     parser.add_argument('-s', '--save_path', dest='save_path', type=str, help="path to save output")
     parser.add_argument("-t", "--threshold", dest='threshold', type=float, default=35, help="blurry threshold")
@@ -175,7 +186,6 @@ if __name__ == '__main__':
     
     for input_path in find_images(args.input_dir):
         try:
-            #logging.info("processing {0}".format(input_path))
             I = cv2.imread(input_path)
             per, blurext = blur_detect(I, args.threshold)
             if per < args.MinZero:
